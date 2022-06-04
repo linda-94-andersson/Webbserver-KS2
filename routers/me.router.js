b2ba31b2bb29fe659796e02866757b023202954f
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-04 18:03:18 +0200

    Simple setup for books routers

---
+++ routers/me.router.js
@@ -0,0 +1,15 @@
+const express = require("express"); 
+const morgan = require("morgan"); 
+
+const booksRouter = express.Router(); 
+
+booksRouter.use(morgan("dev")); 
+
+booksRouter.get("/books"); 
+booksRouter.get("/books/:id"); 
+booksRouter.post("/books"); 
+booksRouter.put("/books/:id"); 
+booksRouter.patch("/books/:id"); 
+booksRouter.delete("/books/:id"); 
+
+module.exports = booksRouter; 
