9e836142cf26d10fd567614dbb2e95b22f58f181
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-04 18:21:05 +0200

    Simple route test for books

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const booksRouter = require("./routers/books.router");
+
 const PORT = process.env.PORT || 5000; 
 
 const app = express(); 
 
 app.use(cors()); 
 app.use(bodyParser.json()); 
+
+app.use(booksRouter);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
