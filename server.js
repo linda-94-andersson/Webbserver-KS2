9734945bc7a0d9063224646c942ab80a0c00aad4
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-04 17:55:12 +0200

    Simple setup for server

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000; 
+
+const app = express(); 
+
+app.use(cors()); 
+app.use(bodyParser.json()); 
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something in the request is incorrect")
+}); 
+
+app.listen(PORT, () => {
+    console.log(`The server runs now at ${PORT}`);
+}); 
