c1f27ac56b006ed4aa3aeec95af209384098185a
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-08 17:17:07 +0200

    Setup of users

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const booksRouter = require("./routers/books.router");
+const usersRouter = require("./routers/users.router");
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(booksRouter);
+app.use(usersRouter);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
