3ff449a09aad4e3869e215689d71d83c2097aede
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-08 12:30:15 +0200

    More functions for books

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,11 +1,9 @@
-const uuid = require("uuid");
-
 const model = require("../models/books.model");
 
 async function getBooks(req, res) {
     const result = await model.findAll();
-    if(!result){
-        return res.status(404).send("Could not get the books"); 
+    if (!result) {
+        return res.status(404).json({ message: "Could not get the books" });
     }
     res.status(200).json(result);
 }
@@ -13,13 +11,29 @@ async function getBooks(req, res) {
 async function getBook(req, res) {
     const result = await model.findOne(req.params.id);
     if (!result) {
-        return res.status(404).send("No ID found");
+        return res.status(404).json({ message: "No ID found" });
     }
     res.status(200).json(result);
 }
 
-function addBook(req, res) {
+async function addBook(req, res) {
+    const { title, author, genre } = req.body;
 
+    if (!title || !author || !genre) {
+        return res.status(400).json({ message: "All data must be send with the request" });
+    }
+    const newBook = {
+        title,
+        author,
+        genre
+    };
+    try {
+        const result = await model.addOne(newBook);
+
+        res.status(201).json({ id: result.lastID, ...newBook });
+    } catch (error) {
+        return res.status(400).json({ message: "Book already exist" });
+    }
 }
 
 function changeBook(req, res) {
@@ -30,8 +44,12 @@ function manageBook(req, res) {
 
 }
 
-function deleteBook(req, res) {
-
+async function deleteBook(req, res) {
+    const result = await model.deleteOne(req.params.id);
+    if (result.changes === 0) {
+        return res.status(404).json({ message: "Book with this ID do not exist" });
+    }
+    res.status(200).json({ message: "Book deleted successfully" });
 }
 
 module.exports = {
--- models/books.model.js
+++ models/books.model.js
@@ -1,5 +1,16 @@
 const db = require("../config/db");
 
+
+/**
+ * {
+ * id: number,
+ * title: string,
+ * author: string,
+ * genre: string,
+ * lendout: boolean // setup later
+ * }
+ */
+
 function findAll() {
     const sql = "SELECT * FROM books";
 
@@ -28,7 +39,49 @@ function findOne(id) {
     })
 }
 
+function addOne(book) {
+    const sql = "INSERT INTO books (title, author, genre) VALUES (?,?,?)";
+
+    return new Promise((resolve, reject) => {
+        db.run(sql, [book.title, book.author, book.genre], function (error) {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve(this);
+        })
+    })
+}
+
+//change = PUT
+function changeOne(book) {
+const sql = "";
+}
+
+//manage = PATCH
+function manageOne() {
+
+}
+
+function deleteOne(id) {
+    const sql = "DELETE FROM books WHERE id = ?";
+
+    return new Promise((resolve, reject) => {
+        db.run(sql, id, function (error) {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve(this);
+        })
+    })
+}
+
 module.exports = {
     findAll,
     findOne,
+    addOne,
+    changeOne,
+    manageOne,
+    deleteOne
 }
