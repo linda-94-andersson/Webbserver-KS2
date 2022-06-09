f146ad9b52e8ce74b00ab54b816535eb6f20a373
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-09 13:34:03 +0200

    Added auth path

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const auth = require("../middlewares/auth");
+
 const usersRouter = express.Router();
 
 const usersController = require("../controllers/users.controller");
@@ -8,10 +10,7 @@ const usersController = require("../controllers/users.controller");
 usersRouter.use(morgan("dev"));
 
 usersRouter.get("/users", usersController.getUsers);
-usersRouter.get("/users/:id", usersController.getUser);
-usersRouter.post("/users", usersController.addUser);
-usersRouter.put("/users/:id", usersController.changeUser);
-usersRouter.patch("/users/:id", usersController.manageUser);
-usersRouter.delete("/users/:id", usersController.deleteUser);
+usersRouter.post("/users/lend", auth, usersController.lendBook);
+usersRouter.post("/users/return", auth, usersController.returnBook);
 
 module.exports = usersRouter; 
