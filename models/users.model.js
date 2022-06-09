65c4779b77d5f053aaa1dbcc909fbc3f9e41a585
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-09 09:15:44 +0200

    findOne & addOne

--- models/users.model.js
+++ models/users.model.js
@@ -14,6 +14,36 @@ function findAll() {
     })
 }
 
+function findOne(email) {
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    return new Promise((resolve, reject) => {
+        db.get(sql, email, (error, rows) => {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+function addOne(user) {
+    const sql = "INSERT INTO users (username, email, password) VALUES (?,?,?)";
+
+    return new Promise((resolve, reject) => {
+        db.run(sql, [user.username, user.email, user.password], (error) => {
+            if (error) {
+                console.error(error.message);
+                reject(error);
+            }
+            resolve();
+        })
+    })
+}
+
 module.exports = {
     findAll,
+    findOne,
+    addOne
 }
