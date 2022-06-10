21e30d0cd50fcd9e78d93ee80644797b06a368d6
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-10 18:24:04 +0200

    Added error handling

--- controllers/me.controller.js
+++ controllers/me.controller.js
@@ -2,6 +2,9 @@ const loanModel = require("../models/loans.model");
 
 async function getMe(req, res) {
     const loans = await loanModel.getUserLoans(req.user.id);
+    if (!loans) {
+        return res.status(400).json({ message: "No data available" });
+    }
 
     res.status(200).json({ user: req.user, loans });
 }
