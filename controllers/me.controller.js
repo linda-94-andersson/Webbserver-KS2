ba454d4c1f7994bf28754cb1f0dd6aed410eabe6
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-13 09:17:54 +0200

    New error message

--- controllers/me.controller.js
+++ controllers/me.controller.js
@@ -3,7 +3,7 @@ const loanModel = require("../models/loans.model");
 async function getMe(req, res) {
     const loans = await loanModel.getUserLoans(req.user.id);
     if (!loans) {
-        return res.status(400).json({ message: "No data available" });
+        return res.status(400).json({ message: "Could not get the loandata" });
     }
 
     res.status(200).json({ user: req.user, loans });
