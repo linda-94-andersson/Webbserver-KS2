a10f22a5f2526f975c32bf7dc47544bef8c3b303
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-13 16:38:09 +0200

    Added 404 for routs that dont exist

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const usersRouter = require("./routers/users.router");
 const authRouter = require("./routers/auth.router");
 const meRouter = require("./routers/me.router");
 
-const logger = require("./middlewares/logger"); 
+const logger = require("./middlewares/logger");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,13 +16,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(logger); 
+app.use(logger);
 
 app.use(booksRouter);
 app.use(usersRouter);
 app.use(authRouter);
 app.use(meRouter);
 
+
+app.all('*', (req, res) =>
+    res.status(404).json({ error: `Route '${req.url}' does not exist` })
+);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: "Something went wrong" });
